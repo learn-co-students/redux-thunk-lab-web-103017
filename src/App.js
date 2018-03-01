2ed3f610bcfdc39cd9539a43e2598b57e0e1e3fe
dansbands <dansbands@hotmail.com>
2018-02-28 20:06:14 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap';
+import CatList from './CatList';
+import { connect } from 'react-redux';
+import * as actions from './actions/catActions';
+import { bindActionCreators } from 'redux';
 
-class App extends Component {   
-  
+
+export class App extends Component {
+
+  componentDidMount() {
+    if (this.props.catPics.length === 0) {
+      console.log('in componentDidMount');
+      this.props.actions.fetchCats()
+    }
+  }
+
+
+
   render() {
+    console.log('App', this.props);
     return (
       <div className="App">
         <Navbar>
@@ -13,12 +28,18 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        <CatList catPics={this.props.catPics} />
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return { catPics: state.cats.pictures }
+}
 
+const mapDispatchToProps = dispatch => {
+  return { actions: bindActionCreators(actions, dispatch)}
+}
 
-export default App
-
+export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App'
+import { WrapperApp } from './App'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+
+
+
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <WrapperApp />
+  </Provider>,
     document.getElementById('root')
 );
